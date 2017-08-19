d6d96efd537d3108fdd2711a1b3c293cb31a875a
muhand <muhandjumah@gmail.com>
2017-08-19 05:27:15 -0400

    Created basic web scraping

---
+++ ChannelsScraper/ChannelsScraper/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ChannelsScraper
+{
+    partial class ChannelsScraper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.channelsDataGridViewer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.channelsDataGridViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // channelsDataGridViewer
+            // 
+            this.channelsDataGridViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.channelsDataGridViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.channelsDataGridViewer.Location = new System.Drawing.Point(0, 0);
+            this.channelsDataGridViewer.Name = "channelsDataGridViewer";
+            this.channelsDataGridViewer.RowTemplate.Height = 24;
+            this.channelsDataGridViewer.Size = new System.Drawing.Size(903, 596);
+            this.channelsDataGridViewer.TabIndex = 0;
+            // 
+            // ChannelsScraper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 596);
+            this.Controls.Add(this.channelsDataGridViewer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ChannelsScraper";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Channels Scraper";
+            this.Load += new System.EventHandler(this.ChannelsScraper_LoadAsync);
+            ((System.ComponentModel.ISupportInitialize)(this.channelsDataGridViewer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView channelsDataGridViewer;
+    }
+}
+
---
+++ ChannelsScraper/ChannelsScraper/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace ChannelsScraper
+{
+
+    public class Channel
+    {
+        public string Name { get; set; }
+        public string URL { get; set; }
+    }
+
+    public partial class ChannelsScraper : Form
+    {
+        private DataTable _channelsDataTable;
+        HtmlWeb _web;
+        public ChannelsScraper()
+        {
+            InitializeComponent();
+            InitTable();
+            _web = new HtmlWeb();
+        }
+
+        private async void ChannelsScraper_LoadAsync(object sender, EventArgs e)
+        {
+            //loadWeb("http://beitsports.blogspot.de/p/adsbygoogle-window_18.html");
+            var channels = await ChannelsFromPage("http://beitsports.blogspot.de/p/adsbygoogle-window_18.html");
+
+            foreach (var channel in channels)
+            {
+                _channelsDataTable.Rows.Add(channel.Name, channel.URL);
+            }
+        }
+
+        private async Task<List<Channel>> ChannelsFromPage(string websiteURL)
+        {
+            var webPageDoc = await Task.Factory.StartNew(() => _web.Load(websiteURL));
+            var nodes = webPageDoc.DocumentNode.SelectNodes("//*[@id=\"Blog1\"]//div//div//div//div//div//div//h2//span//span//a");
+            var channelsName = nodes.Select(node => node.InnerText);
+            var channelsURL = nodes.Select(node => node.Attributes[0].Value);
+
+            //If there are not data in channelsName or channelsURL then return empty list
+            if (channelsName == null || channelsURL == null)
+                return new List<Channel>();
+            
+            return channelsName.Zip(channelsURL, (channelName, channelURL) => new Channel() { Name = channelName, URL = channelURL, }).ToList();
+        }
+
+        /// <summary>
+        /// Initialize DataTable for channels
+        /// </summary>
+        private void InitTable()
+        {
+            _channelsDataTable = new DataTable("ChannelsDataTable");
+            _channelsDataTable.Columns.Add("Channel Name", typeof(string));
+            _channelsDataTable.Columns.Add("Channel URL", typeof(string));
+            channelsDataGridViewer.DataSource = _channelsDataTable;
+        }
+
+
+
+    }
+}
